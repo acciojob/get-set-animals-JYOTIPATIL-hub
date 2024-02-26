c85adec8141294038a6c0b785ccdffedcd1978b5
JYOTIPATIL-hub <142783664+JYOTIPATIL-hub@users.noreply.github.com>
2024-02-26 20:20:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 //complete this code
-class Animal {}
-
-class Dog extends Animal {}
-
-class Cat extends Animal {}
-
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
 class Animal {
     constructor(species) {
         this._species = species;
@@ -22,6 +12,32 @@ class Animal {
         console.log(`The ${this._species} makes a sound`);
     }
 }
+
+class Cat extends Animal {
+    purr() {
+        console.log("purr");
+    }
+}
+
+class Dog extends Animal {
+    bark() {
+        console.log("woof");
+    }
+}
+
+// Example usage:
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // Output: The Siamese makes a sound
+myCat.purr(); // Output: purr
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // Output: The Golden Retriever makes a sound
+myDog.bark(); // Output: woof
+
+   
+
+    
+
 
 class Cat extends Animal {
     purr() {
